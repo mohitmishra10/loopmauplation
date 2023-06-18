6f43a3378854ba1078a674b363fe8481cc1eab80
Mohit Mishra <136458158+mohitmishra10@users.noreply.github.com>
2023-06-18 22:08:39 +0530

    Add files via upload

---
+++ loopmanuplation.py
@@ -0,0 +1,46 @@
+#LOOP MANUPLATION
+#BREAK
+
+# for i in range(1,20):
+#     if i==6: 
+#         break
+# else:
+#         print(i)
+
+#CONTINUE
+
+for i in range(1,20):
+    if i==6: 
+        continue
+    else:
+        print(i)
+
+
+#PASS
+
+for i in range(1,20):
+    if i==6:
+        pass
+    else:
+        print(i)
+
+#FOR WITH ELSE
+
+for i in range(1,20):
+    print(i)
+else:
+    print("FOR WITH ELSE")
+
+#WHILE WITH ELSE
+
+i=0
+while i<=15:
+    print(i)
+    i+=2
+else:
+    print("WHILE WITH ELSE")
+    
+
+
+
+
